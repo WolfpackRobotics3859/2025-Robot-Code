64b551840bd7a812edb4a401811027f079da87ab
Logan Sar <loganjocsonsar@gmail.com>
2025-01-11 12:21:08 -0800

    Created Coral Placer subsytem, 4 new files Can use percentage and voltage

---
+++ Big-Bad-Wolf-2025/src/main/java/frc/robot/constants/Global.java
@@ -0,0 +1,18 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.constants;
+
+/** Add your docs here. */
+public class Global
+{
+    //Different modes any motor can call
+    public static enum MODE
+    {
+        PERCENT,
+        VOLTAGE,
+        POSITION,
+        BRAKE
+    }
+}
