4933c44cbc403e016f78860215b9600a4a814c06
Jason B <144144238+cool-cellist@users.noreply.github.com>
2025-01-14 22:16:56 -0800

    [ISSUE:10] Added Drivetrain Subsystem

--- Big-Bad-Wolf-2025/src/main/java/frc/robot/RobotContainer.java
+++ Big-Bad-Wolf-2025/src/main/java/frc/robot/RobotContainer.java
@@ -4,10 +4,16 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Elevator;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -17,39 +23,59 @@ import frc.robot.subsystems.Elevator;
  */
 public class RobotContainer 
 {
-  // The robot's subsystems and commands are defined here...
-  private final Elevator m_Elevator = new Elevator();
+  public static final CommandSwerveDrivetrain m_Drivetrain = TunerConstants.createDrivetrain(); 
+  public static final Elevator m_Elevator = new Elevator();
+
+  private final CommandXboxController m_DriverController = new CommandXboxController(0);
+  private final CommandXboxController m_CoDriverController = new CommandXboxController(1);
+  
+  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
+          .withDeadband(TunerConstants.MaxSpeed * 0.1).withRotationalDeadband(TunerConstants.MaxAngularRate * 0.1) // Add a 10% deadband
+          .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+
+  private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+
+  public Command forwardDynamicCommand = m_Drivetrain.sysIdDynamic(Direction.kForward);
+  public Command reverseDynamicCommand = m_Drivetrain.sysIdDynamic(Direction.kReverse);
+  public Command forwardQuasistaticCommand = m_Drivetrain.sysIdQuasistatic(Direction.kForward);
+  public Command reverseQuasistaticCommand = m_Drivetrain.sysIdQuasistatic(Direction.kReverse);
+  
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() 
   {
-    // Configure the trigger bindings
     configureBindings();
+    configureCharacterizationBindings(); // REMOVE FROM COMPETITION BUILD (REMOVE_BEFORE_COMP)
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureBindings() 
   {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    //empty for now
+      // Note that X is defined as forward according to WPILib convention,
+      // and Y is defined as to the left according to WPILib convention.
+      m_Drivetrain.setDefaultCommand(
+          // Drivetrain will execute this command periodically
+          m_Drivetrain.applyRequest(() ->
+              drive.withVelocityX(-m_DriverController.getLeftY() * TunerConstants.MaxSpeed) // Drive forward with negative Y (forward)
+                  .withVelocityY(-m_DriverController.getLeftX() * TunerConstants.MaxSpeed) // Drive left with negative X (left)
+                  .withRotationalRate(-m_DriverController.getRightX() * TunerConstants.MaxAngularRate) // Drive counterclockwise with negative X (left)
+          )
+      );
+
+      m_DriverController.a().whileTrue(m_Drivetrain.applyRequest(() -> brake));
+  
+      m_DriverController.leftBumper().onTrue(m_Drivetrain.runOnce(() -> m_Drivetrain.seedFieldCentric()));
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
+  private void configureCharacterizationBindings()
+  {
+    SmartDashboard.putData("Forward Dynamic", this.forwardDynamicCommand);
+    SmartDashboard.putData("Reverse Dynamic", this.reverseDynamicCommand);
+    SmartDashboard.putData("Forward Quasistatic", this.forwardQuasistaticCommand);
+    SmartDashboard.putData("Reverse Quasistatic", this.reverseQuasistaticCommand);
+  }
+
   public Command getAutonomousCommand() 
   {
-    // An example command will be run in autonomous
-    return null;
+      return Commands.print("No autonomous command configured");
   }
 }
