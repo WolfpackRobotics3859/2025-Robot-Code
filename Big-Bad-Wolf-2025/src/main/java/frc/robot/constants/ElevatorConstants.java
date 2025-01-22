6db4f23943de92370b9c03eb4c8cfaca7d20ffbe
Logan Sar <147781687+SuzakuSar@users.noreply.github.com>
2025-01-22 09:53:53 -0800

    [ISSUE:31] Added Elevator Commands

---
+++ Big-Bad-Wolf-2025/src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -0,0 +1,18 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.constants;
+
+/** Add your docs here. */
+public class ElevatorConstants 
+{
+//All values are filler values and units until further testing
+    public static final double ELEVATOR_ZERO_POSITION = 0;
+    public static final double ELEVATOR_LEVEL_TWO = 10;
+    public static final double ELEVATOR_LEVEL_THREE = 20;
+    public static final double ELEVATOR_LEVEL_FOUR = 30;
+
+    public static final double ELEVATOR_UP_VOLTAGE = 4;
+    public static final double ELEVATOR_DOWN_VOLTAGE = -4;
+}
