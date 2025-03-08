3a9fd7314ea98e19d4d41399017a3c9b640d2b0a
sjconstantine <sjconstantine08@gmail.com>
2025-03-08 11:15:39 -0800

    duplicate imports

--- Big-Bad-Wolf-2025/src/main/java/frc/robot/Robot.java
+++ Big-Bad-Wolf-2025/src/main/java/frc/robot/Robot.java
@@ -7,9 +7,7 @@ package frc.robot;
 import com.ctre.phoenix6.SignalLogger;
 
 import edu.wpi.first.wpilibj.DataLogManager;
-import com.ctre.phoenix6.SignalLogger;
 
-import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
